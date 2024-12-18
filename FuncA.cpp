c60ee5b063ad7c7e0bb8c992b1f06cb95775ab24
Alina Petrenko <petrrenkoo@gmail.com>
2024-12-18 19:27:55 +0200

    BranchA: Complete implementation of FuncA with comments.

--- FuncA.cpp
+++ FuncA.cpp
@@ -1,5 +1,20 @@
 #include "FuncA.h"
+#include <cmath>
 
-int FuncA::calculate() {
-    return 0; // Initial implementation
+/**
+ * @brief Calculate the sum of the first n elements of the hyperbolic cosine series.
+ * 
+ * @param x The input value for which the series is calculated.
+ * @param n The number of terms to include in the series sum.
+ * @return double The sum of the first n terms of the series.
+ */
+double FuncA::calculate(double x, int n) {
+    double sum = 0.0;
+    for (int k = 0; k < n; ++k) {
+        double term = std::pow(x, 2 * k) / tgamma(2 * k + 1); // tgamma(n) = (n-1)!
+        sum += term;
+    }
+    return sum;
 }
+
+
--- FuncA.h
+++ FuncA.h
@@ -1,14 +1,20 @@
 #ifndef FUNCA_H
 #define FUNCA_H
 
+/**
+ * @class FuncA
+ * @brief Class to calculate the sum of n terms in the hyperbolic cosine series.
+ */
 class FuncA {
 public:
     /**
-     * @brief Calculate a trigonometric function (initially returns 0).
-     * 
-     * @return int 
+     * @brief Calculate the sum of n terms in the series.
+     * @param x Input value for calculation.
+     * @param n Number of terms to sum.
+     * @return Result of the series calculation.
      */
-    int calculate();
+    double calculate(double x, int n);
 };
 
 #endif // FUNCA_H
+
